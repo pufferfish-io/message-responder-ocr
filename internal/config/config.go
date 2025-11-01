033eca6d4cca3ba0d558d75a7a0fe1dff395c7ae
Shamil Alibekov <shamil@MacBook-Pro-Shamil.local>
2025-11-02 01:56:49 +0300

    chore(docs): update version tag in README from v0.1.1 to v0.0.1 for consistency refactor(config.go): rename environment variable keys for clarity and specificity in the configuration structure

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,23 +9,23 @@ import (
 
 type Doc2text struct {
 	AccessTokenURL string `validate:"required" env:"ACCESS_TOKEN_URL"`
-	ClientID       string `validate:"required" env:"CLIENT_ID"`
-	ClientSecret   string `validate:"required" env:"CLIENT_SECRET"`
+	ClientID       string `validate:"required" env:"CLIENT_ID_MESSAGE_RESPONDER_OCR"`
+	ClientSecret   string `validate:"required" env:"CLIENT_SECRET_MESSAGE_RESPONDER_OCR"`
 	GRpcUrl        string `validate:"required" env:"G_RPC_URL"`
 }
 
 type Kafka struct {
 	BootstrapServersValue string `validate:"required" env:"BOOTSTRAP_SERVERS_VALUE"`
-	GroupID               string `validate:"required" env:"GROUP_ID"`
-	RequestTopicName      string `validate:"required" env:"REQUEST_TOPIC_NAME"`
-	ResponseTopicName     string `validate:"required" env:"RESPONSE_TOPIC_NAME"`
+	GroupID               string `validate:"required" env:"GROUP_ID_MESSAGE_RESPONDER_OCR"`
+	RequestTopicName      string `validate:"required" env:"TOPIC_NAME_OCR_REQUEST"`
+	ResponseTopicName     string `validate:"required" env:"TOPIC_NAME_TG_RESPONSE_PREPARER"`
 	SaslUsername          string `env:"SASL_USERNAME"`
 	SaslPassword          string `env:"SASL_PASSWORD"`
-	ClientID              string `env:"CLIENT_ID"`
+	ClientID              string `env:"CLIENT_ID_MESSAGE_RESPONDER_OCR"`
 }
 type Config struct {
-	Kafka    Kafka    `envPrefix:"MSG_RESP_OCR_KAFKA_"`
-	Doc2text Doc2text `envPrefix:"MSG_RESP_OCR_DOC3TEXT_"`
+	Kafka    Kafka    `envPrefix:"KAFKA_"`
+	Doc2text Doc2text `envPrefix:"DOC3TEXT_"`
 }
 
 func Load() (*Config, error) {
